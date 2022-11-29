857f2f76d91bed25b39a95b004128e8247676053
KSidan <sidan1722@gmail.com>
2022-11-29 19:09:38 +0700

    implemented book manager

--- include/bplustree.h
+++ include/bplustree.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cassert>
 #include <vector>
 #include <string>
 using namespace std;
--- src/book_manager.cpp
+++ src/book_manager.cpp
@@ -4,58 +4,142 @@
 #include "book.cpp"
 using namespace std;
 
-BookManager::BookManager() {
+BookManager::BookManager() 
+{
+	data = BPlusTree<string, Book>();
+}
+
+BookManager::BookManager(const string& file_location) 
+{
+	data = BPlusTree<string, Book>();
+	load_data(file_location);    
+}
+
+void BookManager::load_data(const string& file_location) 
+{
+	
+}
+
+void BookManager::save_data(const string& file_location) 
+{
     
 }
 
-BookManager::BookManager(const string& file_location) {
-    
+int BookManager::get_num_of_books() 
+{
+    return data.get_size();
 }
 
-void BookManager::load_data(const string& file_location) {
-
+vector<Book> BookManager::get_all_books() const 
+{
+    vector<Book*> books_ori = data.get_all_values();
+    vector<Book> books_copy;
+    
+    for (auto it : books_ori)
+    {
+    	books_copy.push_back(Book(*it));
+	}
+	return books_copy;
 }
 
-void BookManager::save_data(const string& file_location) {
-    
+Book BookManager::get_book_by_isbn(string isbn) const 
+{
+    Book* book = data.get_value(isbn);
+    if (book == nullptr)
+    {
+    	return Book();
+	}
+	else 
+	{
+		return Book(*book);
+	}
 }
 
-int BookManager::get_num_of_books() {
-    
+bool BookManager::create_book(const Book& book) 
+{
+    if (data.is_exist(book.get_isbn()))
+    {
+    	return false;
+	}
+	else
+	{
+		data.insert(book.get_isbn(), book);
+		return true;
+	}
 }
 
-vector<Book> BookManager::get_all_books() const {
-    
+bool BookManager::update_book(string old_isbn, const Book& book) 
+{
+    if (!data.is_exist(old_isbn))
+    {
+    	return false;
+	}
+	if (old_isbn != book.get_isbn() && data.is_exist(book.get_isbn()))
+	{
+		return false;
+	}
+	
+	if (!delete_book(old_isbn))
+	{
+		cout << "An Unexpected Error has Occured.\n";
+		return false;
+	}
+	return true;
 }
 
-Book BookManager::get_book_by_isbn(string isbn) const {
-    
+bool BookManager::delete_book(string isbn) 
+{
+    return data.remove(isbn);
 }
 
-bool BookManager::create_book(const Book& book) {
-    
+bool BookManager::add_book_stock(string isbn, int amount) 
+{
+    if (data.is_exist(isbn))
+    {
+    	data.get_value(isbn)->add_book_stock(amount);
+    	return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-bool BookManager::update_book(string old_isbn, const Book& book) {
-    
+bool BookManager::remove_book_stock(string isbn, int amount) 
+{
+    if (data.is_exist(isbn))
+    {
+    	data.get_value(isbn)->remove_available_book_stock(amount);
+    	return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-bool BookManager::delete_book(string isbn) {
-    
+bool BookManager::borrow_book(string isbn, int amount) 
+{
+    if (data.is_exist(isbn))
+    {
+    	data.get_value(isbn)->borrow_book(amount);
+    	return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-bool BookManager::add_book_stock(string isbn, int amount) {
-    
-}
-
-bool BookManager::remove_book_stock(string isbn, int amount) {
-    
-}
-
-bool BookManager::borrow_book(string isbn, int amount) {
-    
-}
-
-bool BookManager::return_book(string isbn, int amount) {
-    
+bool BookManager::return_book(string isbn, int amount) 
+{
+    if (data.is_exist(isbn))
+    {
+    	data.get_value(isbn)->return_book(amount);
+    	return true;
+	}
+	else
+	{
+		return false;
+	}
 }
